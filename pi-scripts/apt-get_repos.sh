ee07f7f1dd76b10d5e9a40de465aa10afa9d0159
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-03-06 10:44:10 -0900

    Update apt-get_repos.sh

---
+++ pi-scripts/apt-get_repos.sh
@@ -0,0 +1,18 @@
+sudo sh -c 'deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" >> /etc/apt/sources.list'
+
+sudo apt-get update 
+
+wget https://cran.r-project.org/src/contrib/R6_2.2.0.tar.gz
+wget https://cran.r-project.org/src/contrib/stringr_1.2.0.tar.gz
+wget https://cran.r-project.org/src/contrib/jsonlite_1.3.tar.gz
+wget https://cran.r-project.org/src/contrib/httpuv_1.3.3.tar.gz
+git clone https://github.com/trestletech/plumber.git
+
+sudo apt-get -y install  r-base gpsd gpspipe git
+
+R CMD INSTALL R6_2.2.0.tar.gz
+R CMD INSTALL stringr_1.2.0.tar.gz
+R CMD INSTALL jsonlite_1.3.tar.gz
+R CMD INSTALL httpuv_1.3.3.tar.gz
+sudo su - -c "R -e \"install.packages("/home/pi/plumber", repos = NULL, type='source')\""
+
