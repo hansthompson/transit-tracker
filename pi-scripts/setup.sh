8ac6c36e01c143d9ba94f0e09b3230ad38c3b6ab
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-03-07 12:03:43 -0900

    Update setup.sh

--- pi-scripts/setup.sh
+++ pi-scripts/setup.sh
@@ -8,13 +8,15 @@ wget https://cran.r-project.org/src/contrib/jsonlite_1.3.tar.gz
 wget https://cran.r-project.org/src/contrib/httpuv_1.3.3.tar.gz
 git clone https://github.com/trestletech/plumber.git
 
-sudo apt-get -y install  r-base gpsd gpspipe git
+sudo apt-get -y install  r-base gpsd gpspipe git libcurl4-gnutls-dev libxml2-dev libssl-dev
 
 R CMD INSTALL R6_2.2.0.tar.gz
 R CMD INSTALL stringr_1.2.0.tar.gz
 R CMD INSTALL jsonlite_1.3.tar.gz
 R CMD INSTALL httpuv_1.3.3.tar.gz
 sudo su - -c "R -e \"install.packages("/home/pi/plumber", repos = NULL, type='source')\""
+#sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
+#sudo su - -c "R -e \"devtools::install_github('trestletech/plumber')\""
 
 sudo apt-get install nginx
 sudo /etc/init.d/nginx start
