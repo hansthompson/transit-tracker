263c4a2d0f595000da910bc19e828f90d7bbb1e8
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-03-08 10:51:48 -0900

    Update setup.sh

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -1,1 +1,6 @@
-
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install r-base r-base-dev nginx
+sudo systemctl restart nginx
