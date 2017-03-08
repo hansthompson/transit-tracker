dd69f70608e108f908a9a61ac69766357629377e
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-03-08 11:05:37 -0900

    Update setup.sh

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -3,4 +3,3 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install r-base r-base-dev nginx git libcurl4-gnutls-dev libxml2-dev libssl-dev
-sudo systemctl restart nginx 
