15b9677df14fdb636ef2e1d477fd203ebd66ead7
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-03-08 15:56:24 -0900

    Update setup.sh

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -3,3 +3,4 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install r-base r-base-dev nginx git libcurl4-gnutls-dev libxml2-dev libssl-dev
+sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.us.r-project.org'))\""
