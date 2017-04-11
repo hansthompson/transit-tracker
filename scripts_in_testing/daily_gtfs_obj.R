9f33eb66175095d21d1e30aeaa231ba2e8674bf1
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-04-10 21:11:49 -0800

    Update daily_gtfs_obj.R

--- scripts_in_testing/daily_gtfs_obj.R
+++ scripts_in_testing/daily_gtfs_obj.R
@@ -1,7 +1,7 @@
 library(gtfsdrilldown);library(gtfsr)
 # Filter the gtfs object to just the current day. 
 
-daily_gtfs_obj <- function() {
+daily_gtfs_obj <- function(day = Sys.Date() {
   gtfs_obj <- import_gtfs("https://transitfeeds.com/link?u=http://gtfs.muni.org/People_Mover.gtfs.zip")
-  gtfs_drilldown(gtfs_obj, today = structure(17243, class = "Date"))
+  gtfs_drilldown(gtfs_obj, day)
 }
