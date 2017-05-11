ae4e9425c3672038c42181f049509440274de021
Charles Hans Thompson <hans.thompson1@gmail.com>
2017-05-11 10:37:51 -0800

    Update setup.sh

--- server-scripts/setup.sh
+++ server-scripts/setup.sh
@@ -9,9 +9,9 @@ sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.us.r-project.o
 sudo su - -c "R -e \"install.packages('XML', repos='http://cran.us.r-project.org')\""
 sudo su - -c "R -e \" library(devtools);install_github('ropensci/gtfsr')  \""
 sudo su - -c "R -e \" library(devtools);install_github('hansthompson/dlayr')  \""
-(crontab -l 2>/dev/null; echo "5 0 * * * Rscript /root/transit-tracker/scripts_in_testing/download_gtfs.R ") | crontab -
-(crontab -l 2>/dev/null; echo "* * * * * Rscript /root/transit-tracker/scripts_in_testing/get_tidy_gps.R ") | crontab -
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.3.838-amd64.deb
 sudo gdebi -n shiny-server-1.5.3.838-amd64.deb 
 sudo sed -i 's/  listen 3838;/  listen 80;/' /etc/shiny-server/shiny-server.conf
+(crontab -l 2>/dev/null; echo "5 0 * * * Rscript /root/transit-tracker/scripts_in_testing/download_gtfs.R ") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * Rscript /root/transit-tracker/scripts_in_testing/get_tidy_gps.R ") | crontab -
 # then reboot
